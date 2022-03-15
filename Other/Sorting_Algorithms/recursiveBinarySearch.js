46123becb96faf010d93fb1f57bf795bc11a5406
tom ames <tsames1@gmail.com>
2022-03-15 12:30:32 -0700

    the recursive binary search is now working properly

--- Other/Sorting_Algorithms/recursiveBinarySearch.js
+++ Other/Sorting_Algorithms/recursiveBinarySearch.js
@@ -3,28 +3,28 @@
 //  if the element is not found, return -1
 //  if the element is found, return the index at which it was found
 
-function recursiveBinarySearch(arr, element, pivot = Math.round((arr.length - 1) / 2), index = Math.round((arr.length - 1) / 2)) {
+function recursiveBinarySearch(arr, element, pivot = (arr.length - 1) / 2 | 0, index = (arr.length - 1) / 2 | 0) {
   //--- Base Case Check ---
   if (arr[pivot] === element) {
     return index
   } else if (arr.length === 1 && arr[0] != element) {
     return -1
   // --- Calls on Subsections of the Array ---
+  //Call if element is greater than pivot
   } else if (arr[pivot] < element) {
     newArr = arr.slice(pivot + 1, arr.length)
     console.log(`Element (${element}) is bigger than pivot (${arr[pivot]}) in ${arr}. The new array is ${newArr}.`);
-    pivot = Math.round((newArr.length - 1) / 2);
-    index += pivot
-    console.log(`The new pivot is ${pivot} and the new index is ${index}`);
-    console.log(`Making another call...`)
+    pivot = (newArr.length - 1) / 2 | 0;
+    index += pivot + 1
+    console.log(`Making another call with pivot (${newArr[pivot]}) and index (${index})`)
     return recursiveBinarySearch(newArr, element, pivot, index)
   } else {
+    //Call if element is less than pivot
     newArr = arr.slice(0, pivot);
     console.log(`Element (${element}) is smaller than pivot (${arr[pivot]}) in ${arr}. The new array is ${newArr}.`);
-    pivot = Math.round((newArr.length - 1) / 2);
-    index -= (newArr.length - 1) - pivot
-    console.log(`The new pivot is ${pivot} and the new index is ${index}`);
-    console.log(`Making another call...`)
+    pivot = (newArr.length - 1) / 2 | 0;
+    index -= (newArr.length) - pivot
+    console.log(`Making another call with pivot (${newArr[pivot]}) and index (${index})`)
     return recursiveBinarySearch(newArr, element, pivot, index)
   }
 }
