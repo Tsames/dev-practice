2b33f5e0139a4c52f33808a5f228726f95800981
tom ames <tsames1@gmail.com>
2022-12-09 17:27:33 -0800

    Updated rotateArray.js with a different solution.

--- LeetCode/Easy/Array/Rotate_Array/rotateArray.js
+++ LeetCode/Easy/Array/Rotate_Array/rotateArray.js
@@ -1,12 +1,18 @@
 /* Given an array, rotate the array to the right by k steps, where k is non-negative. */
 
+
+//Solution with extra space
 function rotateArray(nums, k) {
 
-  for(let i = 0; i < k; i++) {
-    nums.unshift(nums.pop())
+  const output = [];
+
+  for (let i = 0; i < nums.length; i++) {
+
+    output.push(nums[(nums.length - (k) + i) % nums.length]);
+
   }
 
-  return nums;
+  return output;
 }
 
 module.exports = rotateArray
