7895fbdfd0b39e6766681b176fd29dbfdb1eec18
tom <tsames1@gmail.com>
2023-01-25 20:08:39 -0800

    reverse.js and reverse.test.js completed.

--- LeetCode/Easy/Strings/Reverse_Integer/reverse.js
+++ LeetCode/Easy/Strings/Reverse_Integer/reverse.js
@@ -4,7 +4,21 @@ causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], 
 Assume the environment does not allow you to store 64-bit integers (signed or unsigned).*/
 
 const reverse = (x) => {
-    
+    let output = "";
+    let temp = x.toString()
+    for (let i=temp.length - 1; i >= 0; i--) {
+        if (temp[i] === "-") {
+            output = temp[i] + output;
+        } else {
+            output += temp[i];
+        }
+    }
+
+    if ((Number(output) < -2147483648) || (Number(output) > 2147483647)) {
+        return 0;
+    } else {
+        return Number(output);
+    }
 }
 
 module.exports = reverse
