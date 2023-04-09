0e092e5786d71546541629be839cfb6f39b82bdb
tom <tsames1@gmail.com>
2023-04-08 22:46:40 -0700

    added typescript to leetcode scripts

---
+++ LeetCode/Easy/Strings/Implement strStr()/strStr.ts
@@ -0,0 +1,10 @@
+/* Given two strings needle and haystack, return the index of the first occurrence
+of needle in haystack, or -1 if needle is not part of haystack. */
+
+function strStr(haystack:string, needle:string) :number {
+
+    
+
+
+    return 0;
+}
