9977f7698ab462a8d86f71494bae2355d83ecc13
tom ames <tsames1@gmail.com>
2023-04-26 10:42:10 -0700

    Continuing to improve SinglyLinkedLists.py

--- Python/Data_Structures/SinglyLinkedLists.py
+++ Python/Data_Structures/SinglyLinkedLists.py
@@ -11,6 +11,8 @@ class ListNode:
 # ------------ Linked List Class ------------
 
 class LinkedList:
+
+  # ---%%%--- Constructor ---%%%---
   def __init__(self, nodeValues = []):
     self.head = None
     self.length = 0
@@ -18,14 +20,7 @@ class LinkedList:
     for value in nodeValues:
       self.append(value)
 
-  # def length(self):
-  #   size = 0
-  #   currentNode = self.head
-  #   while (currentNode != None):
-  #     size += 1
-  #     currentNode = currentNode.next
-  #   return size
-
+  # ---%%%--- Get and return the last node in the list ---%%%---
   def getlastNode(self):
     if (self.head == None):
       return None
@@ -35,11 +30,13 @@ class LinkedList:
         currentNode = currentNode.next
       return currentNode
     
-  def getNode(self, n):
-    if (self.head == None):
+  # ---%%%--- Get and return the nth node in the list ---%%%---
+  #Optional node argument that, if supplied, will retrieve the nth node from node
+  def getNode(self, n, node = None):
+    if (node == None and self.head == None):
       return None
     else:
-      currentNode = self.head
+      currentNode = self.head if node == None else node
       for i in range(n - 1):
         if currentNode != None:
           currentNode = currentNode.next
@@ -47,6 +44,7 @@ class LinkedList:
           break
     return currentNode
 
+  # ---%%%--- Add a new node to the end of the list, return nothing ---%%%---
   def append(self, value):
     newNode = ListNode(value, None)
     self.length += 1
@@ -56,6 +54,7 @@ class LinkedList:
       lastNode = self.getlastNode()
       lastNode.next = newNode
 
+  # ---%%%--- Add a new node to the beginning of the list, return nothing ---%%%---
   def prepend(self, value):
     self.length += 1
     if (self.head == None):
@@ -65,6 +64,45 @@ class LinkedList:
       newNode = ListNode(value, self.head)
       self.head = newNode
 
+  # ---%%%--- Remove and return last node ---%%%---
+  def pop(self):
+    if (self.length == 0):
+      return
+    elif (self.length == 1):
+      output = self.head
+      self.head = None
+      self.length -= 1
+      return output
+    else:
+      newLastNode = self.getNode(self.length - 1)
+      lastNode = newLastNode.next
+      newLastNode.next = None
+      self.length -= 1
+      return lastNode
+    
+  # ---%%%--- Remove and return head node ---%%%---
+  def removeHead(self):
+    if (self.head == None):
+      return
+    else:
+      oldHead = self.head
+      self.head = self.head.next
+      self.length -= 1
+      return oldHead
+    
+  # ---%%%--- Remove and return the nth node ---%%%---
+  #Optional node argument, that if supplied, will remove the nth node from node
+  def remove(self, n, node = None):
+    targetStart = self.head if node == None else node
+    if targetStart == None:
+      return None
+    else:
+      targetNode = self.getNode(n, node)
+      if(targetNode)
+
+
+
+  # ---%%%--- String Representation of Linked List ---%%%---
   def __str__(self):
     output_string = ""
     currentNode = self.head
@@ -74,12 +112,14 @@ class LinkedList:
     return output_string
 
 new_linked_list = LinkedList([1, 2, 3, 4, 5])
-print(new_linked_list) # (1) -> (2) -> (3) -> (4) -> (5) ->
+# print(new_linked_list) # (1) -> (2) -> (3) -> (4) -> (5) ->
 # print(new_linked_list.length) #5
 # print(new_linked_list.getlastNode()) # (5) -> None
 # print(new_linked_list.getNode(3)) #(3) -> (4)
 # print(new_linked_list.getNode(100)) #None
-print(new_linked_list.prepend(0))
-print(new_linked_list.length)
-print(new_linked_list)
+new_linked_list.prepend(0)
+# print(new_linked_list) #(0) -> (1) -> (2) -> (3) -> (4) -> (5) ->
+# print(new_linked_list.pop()) #(5) -> None
+# print(new_linked_list) #(0) -> (1) -> (2) -> (3) -> (4) ->
+
 
