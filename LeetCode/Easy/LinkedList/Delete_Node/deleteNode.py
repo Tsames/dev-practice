e698017344ab2b6e4af72a4f433fe3436d73068c
tom ames <tsames1@gmail.com>
2023-04-27 17:49:45 -0700

    Additional learning and adjustments to Python Linked List data structure. Began to learn how to import other modules.

---
+++ LeetCode/Easy/LinkedList/Delete_Node/deleteNode.py
@@ -0,0 +1,7 @@
+#Import Linked List Data Structure
+import sys
+sys.path.append("/Users/tomames/dev/practice_repo/Python/Data_Structures/")
+from Linked_Lists import ListNode, LinkedList
+
+newNode = ListNode(5, None)
+print(newNode)
