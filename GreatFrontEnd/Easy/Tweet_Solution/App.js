bfd4295d1706e0b4bf6bf29a6fe98fb120dcb5e5
tom ames <tsames1@gmail.com>
2023-05-02 11:17:47 -0700

    Set up Tweet_Solution folder. This challenge is done using react style code. I am just going to record my code without setting up the environment to test the code.

---
+++ GreatFrontEnd/Easy/Tweet_Solution/App.js
@@ -0,0 +1,55 @@
+/*
+
+Write HTML and CSS to build Twitter's tweet UI.
+
+Tweet demo
+
+Follow the given specifications as closely as you can. An example thumbnail and icon SVGs are provided to you in the template.
+
+Requirements:
+Tweet specifications
+
+1. Tweet:
+Background color: #fff
+Border color: #cfd9de
+Border width: 1px
+Border radius: 16px
+Padding: 12px
+
+2. Thumbnail:
+Size: 48px
+Fully rounded
+
+3. Name:
+Color: #000
+Font family: sans-serif
+Font size: 15px
+Font weight: bold
+
+4. Metadata:
+Color: #71767b
+Font family: sans-serif
+Font size: 15px
+Use &middot; for the dot
+
+5. Message:
+Color: #0f1419
+Font family: sans-serif
+Font size: 15px
+
+6. Actions Row:
+Max width: 425px
+Equal spacing between each action
+
+7. Icon:
+Color: #536471
+Size: 16px
+Vertically centered with the count
+
+8. Action Count:
+Color: #536471
+Font size: 13px
+
+*/
+
+import './styles.css';
