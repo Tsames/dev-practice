25e04c9b834914b304a36e56209fea34ccde8a4e
tom ames <tsames1@gmail.com>
2023-05-06 15:30:21 -0700

    Added addTwoNumbers.py and associated empty test script.

---
+++ Python/LeetCode/Linked_Lists/Medium/Add_Two_Numbers/addTwoNumbers.py
@@ -0,0 +1,54 @@
+#Import Linked List Data Structure
+import sys
+sys.path.append("/Users/tomames/dev/practice_repo/Python/Data_Structures/")
+from Linked_Lists import ListNode, LinkedList
+
+# You are given two non-empty linked lists representing two non-negative integers.
+# The digits are stored in reverse order, and each of their nodes contains a single digit.
+# Add the two numbers and return the sum as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+# Example 1:
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+
+# Example 2:
+# Input: l1 = [0], l2 = [0]
+# Output: [0]
+
+# Example 3:
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# Output: [8,9,9,9,0,0,0,1]
+ 
+
+# Constraints:
+
+# The number of nodes in each linked list is in the range [1, 100].
+# 0 <= Node.val <= 9
+# It is guaranteed that the list represents a number that does not have leading zeros.
+
+def addTwoNumbers(l1, l2):
+  numberOne = ""
+  numberTwo = ""
+
+  while (l1):
+    numberOne = str(l1.value) + numberOne
+    l1 = l1.next
+  
+  while (l2):
+    numberTwo = str(l2.value) + numberTwo
+    l2 = l2.next
+
+  addedNum = int(numberOne) + int(numberTwo)
+  resultingList = [int(i) for i in str(addedNum)[::-1]]
+
+  return LinkedList(resultingList)
+  
+
+testList = LinkedList([1,5,1,5])
+testList2 = LinkedList([2,2,2])
+
+print(addTwoNumbers(testList.head, testList2.head))
+
---
+++ Python/LeetCode/Linked_Lists/Medium/Add_Two_Numbers/test_addTwoNumbers.py
@@ -0,0 +1,9 @@
+import unittest
+from addTwoNumbers import addTwoNumbers
+
+#Import Linked List Data Structure
+import sys
+sys.path.append("/Users/tomames/dev/practice_repo/Python/Data_Structures/")
+from Linked_Lists import ListNode, LinkedList
+
+
