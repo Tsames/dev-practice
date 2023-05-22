de907cebac5554a47c5b35f790c21bc6bd9b75d8
tom ames <tsames1@gmail.com>
2023-05-22 10:51:34 -0700

    Completed test script for lengthOfLongestSubstring.py

--- Python/LeetCode/Arrays/Medium/Longest_Substring_Without_Repeating_Characters/test_lengthOfLongestSubstring.py
+++ Python/LeetCode/Arrays/Medium/Longest_Substring_Without_Repeating_Characters/test_lengthOfLongestSubstring.py
@@ -4,7 +4,12 @@ from lengthOfLongestSubstring import lengthOfLongestSubstring
 class TestRotate(unittest.TestCase):
     
   def test_lengthOfLongestSubstring(self):
-    pass
+    self.assertEquals(lengthOfLongestSubstring("pwwkew"), 3)
+    self.assertEquals(lengthOfLongestSubstring("bbbbb"), 1)
+    self.assertEquals(lengthOfLongestSubstring("b"), 1)
+    self.assertEquals(lengthOfLongestSubstring("abcabcbb"), 3)
+    self.assertEquals(lengthOfLongestSubstring(""), 0)
+
 
 
 if (__name__ == '__main__'):
