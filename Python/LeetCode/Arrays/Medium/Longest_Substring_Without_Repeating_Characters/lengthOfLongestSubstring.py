7514afdd316e76e14e36dd4000226c2a732ef753
tom ames <tsames1@gmail.com>
2023-05-22 11:00:51 -0700

    Added note about more optimal LeetCode answer.

--- Python/LeetCode/Arrays/Medium/Longest_Substring_Without_Repeating_Characters/lengthOfLongestSubstring.py
+++ Python/LeetCode/Arrays/Medium/Longest_Substring_Without_Repeating_Characters/lengthOfLongestSubstring.py
@@ -36,3 +36,16 @@ def lengthOfLongestSubstring(s):
     output = len(result) if len(result) > output else output
 
   return output
+
+# Faster version from LeetCode:
+
+# def lengthOfLongestSubstring(self, s):
+  # used = {}
+  # max_length = start = 0
+  # for i, c in enumerate(s):
+  #   if c in used and start <= used[c]:
+  #     start = used[c] + 1
+  #   else:
+  #     max_length = max(max_length, i - start + 1)
+  #   used[c] = i
+  # return max_length
