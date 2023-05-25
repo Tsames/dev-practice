8090816a5abae44db3cc093a8b8fe69496e9aa55
tom ames <tsames1@gmail.com>
2023-05-24 21:02:07 -0700

    Still working out longestPalindrome.py

--- Python/LeetCode/Arrays/Medium/Longest_Palindromic_Substring/longestPalindrome.py
+++ Python/LeetCode/Arrays/Medium/Longest_Palindromic_Substring/longestPalindrome.py
@@ -19,32 +19,36 @@ def longestPalindrome(str):
 
   #This problem is complicated by identifying the pivot. The palindrome could have a repeating pattern such as
   # "ababab-c-bababa" so its not enough to merely identify if a character that previously appeared in the patern is appearing again.
-  pass
 
-  #The Longest Substring Palindrome possible would be the entire string. This means that the first n/2
-  #characters would form the first half of the pattern where n is the length of the string.
-
-  #To make a palindrome from any one index there would have to be that same character later in the array.
-  #We could use a pointer that resets on each iteration to try and find the biggest index where that character appears again.
-
-  #Iterate through array
-  #If we are at an index
+  #We could iterate through the array (i).
+    #First, check and see if the length of the longest palindrome so far > the remaining number of indicies
+      #If so, return the longest palindrome so far
+    #Second, iterate from the back of array (j) searching for an occurence (str[j]) of the value at the current index (str[i]).
+      #Once found, set pointers at i and j. A palindrome is a mirrored string, so check if the values match for both pointer.
+      #If they don't, then find the next occurence of the value at the current index - if there are no more, continue in the original loop.
+      #If they do match, then move the pointers and compare again. Stop checking the pointers when they either cross or are at the same index.
+      #If it makes it through the second loops then we've found a palindrome.
 
   longest = ""
 
   for i, c in enumerate(str):
-    
-    #Find the latest position where the character is repeated
-    pointerLast = len(str) - 1
-    pointerStart = i
-    while (str[pointerLast] != c or pointerLast <= i):
-      pointerLast -= 1
 
-    check = True
-    #Check if this is a palindrome:
-    while (check and pointerStart <= pointerLast):
-      if (str[pointerStart] != str[pointerLast]):
-        check = False
-      pointerStart += 1
-      pointerLast -= 1
-    if check
+    #Check that the length of the longest palindrome is not greater than the number of remaining indicies.
+    if (len(longest) > len(str) - (i + 1)):
+      print(f"The longest palindrome so far is {longest} with a length of {len(longest)}. Stopping function at index {i} in {str}.")
+      break
+
+    helperPointer = len(str) - 1
+
+    #Iterate from the back of the string, looking for a value at the new index that matches c
+    while (helperPointer > i and helperPointer - i > len(longest)):
+
+      #When you find one, execute a third iteration.
+      if (str[pointerLast] == c):
+
+        check = True
+        while(check and pointerStart < pointerLast):
+          if (str[pointerStart] != str[pointerLast]):
+            check = False
+          pointerStart += 1
+          pointerLast -= 1
