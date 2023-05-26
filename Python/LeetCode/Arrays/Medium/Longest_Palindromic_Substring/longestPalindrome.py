529b85dc1532069dd0e925703300ec49ffd7a3e0
tom ames <tsames1@gmail.com>
2023-05-26 10:47:05 -0700

    Completed and submitted longestPalindrome.py

--- Python/LeetCode/Arrays/Medium/Longest_Palindromic_Substring/longestPalindrome.py
+++ Python/LeetCode/Arrays/Medium/Longest_Palindromic_Substring/longestPalindrome.py
@@ -29,7 +29,7 @@ def longestPalindrome(str):
       #If they do match, then move the pointers and compare again. Stop checking the pointers when they either cross or are at the same index.
       #If it makes it through the second loops then we've found a palindrome.
 
-  longest = "" if len(str) <= 0 else str[0]
+  longest = "" if len(str) == 0 else str[0]
 
   for i, c in enumerate(str):
 
@@ -44,11 +44,8 @@ def longestPalindrome(str):
   return longest
 
 def helper(s, inner, outter):
-  while (inner > 0 and outter < len(s) and s[inner] == s[outter]):
-    print(f"Checking from value(index): {s[inner]}({inner}) to {s[outter]}({outter}).")
+  while (inner >= 0 and outter < len(s) and s[inner] == s[outter]):
+    # print(f"Checking from value(index): {s[inner]}({inner}) to {s[outter]}({outter}).")
     inner -= 1; outter += 1
-  print(f"Returning palindrome: {s[inner+1:outter]}")
+  # print(f"Returning palindrome: {s[inner+1:outter]}")
   return s[inner + 1:outter]
-
-
-print(longestPalindrome("bb"))
--- Python/LeetCode/Arrays/Medium/Longest_Palindromic_Substring/test_longestPalindrome.py
+++ Python/LeetCode/Arrays/Medium/Longest_Palindromic_Substring/test_longestPalindrome.py
@@ -6,12 +6,6 @@ class TestRotate(unittest.TestCase):
   def test_lengthOfLongestSubstring(self):
     self.assertEquals(longestPalindrome("babad"), "bab")
     self.assertEquals(longestPalindrome("cbbd"), "bb")
-    
-    # self.assertEquals(longestPalindrome("b"), 1)
-    # self.assertEquals(longestPalindrome("abcabcbb"), 3)
-    # self.assertEquals(longestPalindrome(""), 0)
-
-
 
 if (__name__ == '__main__'):
   unittest.main()  
