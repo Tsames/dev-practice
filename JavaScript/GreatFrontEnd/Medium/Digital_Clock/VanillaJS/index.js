a4523f4e17a9911489f4bc3f53c530267ae30d4b
Tom Ames <tsames1@gmail.com>
2023-09-03 13:14:30 -0700

    Working on index.js in the VanillaJS implementation of Digital_Clock.

---
+++ JavaScript/GreatFrontEnd/Medium/Digital_Clock/VanillaJS/index.js
@@ -0,0 +1,42 @@
+//Get the Current Time in JavaScript Date Object
+
+const current = new Date()
+
+let hour = null
+let minute = null
+let second = null
+
+const setUpClock = () => {
+    const current = new Date()
+    hour = current.getHours()
+    minute = current.getMinutes()
+    second = current.getSeconds()
+}
+
+const updateClock = () => {
+    second += 1
+
+    if (second >= 60) {
+        minute += 1
+        second = 0
+    }
+
+    if (minute >= 60) {
+        hour += 1
+        minute = 0
+    }
+
+    if (hour > 24) {
+        hour = 1
+    }
+}
+
+const printClock = () => {
+    console.log(`${hour} : ${minutes} : ${seconds}`)
+}
+
+
+setUpClock()
+while (true) {
+    set
+}
