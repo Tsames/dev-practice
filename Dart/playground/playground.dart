b6551a34c6807b6c4bd811a4af8e3a1e70779f48
Tom Ames <tsames1@gmail.com>
2023-10-05 13:12:47 -0700

    Learning Dart.

---
+++ Dart/playground/playground.dart
@@ -0,0 +1,8 @@
+//Every app requires a top-level main() function. This is where execution starts.
+void main() { //Functions that don't explicitly return a value have the void return type.
+  print('Hello World!');
+}
+
+
+//Variables
+//Many variables
