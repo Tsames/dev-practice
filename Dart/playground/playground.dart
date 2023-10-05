70e8e751562bb68a0edecd0802cb5d7ba25e86e2
Tom Ames <tsames1@gmail.com>
2023-10-05 13:17:15 -0700

    Covered main function and variables.

--- Dart/playground/playground.dart
+++ Dart/playground/playground.dart
@@ -1,8 +1,19 @@
 //Every app requires a top-level main() function. This is where execution starts.
-void main() { //Functions that don't explicitly return a value have the void return type.
+void main() {
+  //Functions that don't explicitly return a value have the void return type.
   print('Hello World!');
 }
 
-
 //Variables
-//Many variables
+//Many variables you can declare using var and do not have to explicity define their type.
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
+
+//This is called type inference
+//You can also declare variables with the cosnt and final keywords.
