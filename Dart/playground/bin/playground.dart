e861361389e2e90161c7afc2f83101932ad5052a
Tsames <tsames1@gmail.com>
2024-07-24 18:18:49 -0700

    Cleaning Up

--- Dart/playground/bin/playground.dart
+++
@@ -1,5 +0,0 @@
-import 'package:playground/playground.dart' as playground;
-
-void main(List<String> arguments) {
-  print('Hello world: ${playground.calculate()}!');
-}
--- Rust/rust-playground/target/debug/deps/rust_playground-552c65d8c574bccf.d
+++
@@ -1,5 +0,0 @@
-/Users/tom/dev/dev-practice/Rust/rust-playground/target/debug/deps/rust_playground-552c65d8c574bccf: src/main.rs
-
-/Users/tom/dev/dev-practice/Rust/rust-playground/target/debug/deps/rust_playground-552c65d8c574bccf.d: src/main.rs
-
-src/main.rs:
