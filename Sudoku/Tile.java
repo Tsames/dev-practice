0e5939045e76709c2ff54f6f113444506b1a451c
Tom Ames <tsames1@gmail.com>
2024-08-01 13:58:54 -0700

    Updated main classes to generate a manually defined sudoku board.

--- Sudoku/Tile.java
+++ Sudoku/Tile.java
@@ -1,50 +1,90 @@
 package Sudoku;
 
-public class Tile {
-    public final int id;
+class TileValue {
     public int value;
-    public final int sqr;
-    public final int col;
-    public final int row;
+    public boolean display;
 
-    public Tile(int tileId, int value) {
-        this.id = tileId;
+    TileValue(int value, boolean display) {
         this.value = value;
-        this.sqr = calculateSqr(tileId);
-        this.col = calculateCol(tileId);
+        this.display = display;
+    }
+}
+
+public class Tile {
+    private final int id;
+    private int value;
+    private boolean display;
+    private final int square;
+    private final int column;
+    private final int row;
+
+    public Tile(int tileId, TileValue tileValue) {
+        this.id = tileId;
+        this.value = tileValue.value;
+        this.display = tileValue.display;
+        this.square = calculateSquare(tileId);
+        this.column = calculateColumn(tileId);
         this.row = calculateRow(tileId);
     }
 
-    public Tile(int tileId) {
-        this(tileId, (int) 0);
+    public int getId() {
+        return this.id;
     }
 
-    private int calculateSqr(int tileId) {
-        return (int) (tileId / 9);
+    public boolean getDisplay() {
+        return this.display;
     }
 
-    private int calculateCol(int tileId) {
+    public void setDisplay(boolean showValue) {
+        this.display = showValue;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getSquare() {
+        return this.square;
+    }
+
+    public int getColumn() {
+        return this.column;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getPositionInSquare() {
+        return this.id % 9;
+    }
+
+    private int calculateSquare(int tileId) {
+        return tileId / 9;
+    }
+
+    private int calculateColumn(int tileId) {
         // Calculate the square's column
-        int sqrModulo = (int) (this.sqr % 3);
+        int sqrModulo = this.square % 3;
 
         // Calculate the number of columns over from the first tile in the square
-        int tilesFromFirstTile = (int) ((tileId % 9) % 3);
+        int tilesFromFirstTile = (tileId % 9) % 3;
 
-        return (int) ((sqrModulo * 3) + tilesFromFirstTile);
+        return (sqrModulo * 3) + tilesFromFirstTile;
     }
 
     private int calculateRow(int tileId) {
         // Calculate the square's row
-        int sqrRow = (int) (this.sqr / 3);
+        int sqrRow = this.square / 3;
 
         // Calculate the number of rows over from the first tile in the square
-        int tileRow = (int) ((tileId % 9) / 3);
+        int tileRow = (tileId % 9) / 3;
 
-        return (int) ((sqrRow * 3) + tileRow);
+        return (sqrRow * 3) + tileRow;
     }
 
     @Override
     public String toString() {
-        return "Tile [" + value + "] - Square: " + sqr + " / Row: " + row + " / Column: " + col;
+        return "Tile [" + value + "] - Square: " + square + " / Row: " + row + " / Column: " + column;
     }
 }
