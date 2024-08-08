c17d856760c349ab8c084cdaaa492a44ab95113b
Tsames <tsames1@gmail.com>
2024-08-08 12:56:18 -0700

    Strictly limiting one class per file.

--- Sudoku/Tile.java
+++ Sudoku/Tile.java
@@ -1,33 +1,5 @@
 package Sudoku;
 
-class TileValue {
-    private int number;
-    private boolean display;
-
-    TileValue(int number, boolean display) {
-        if (number < 1 || number > 9) this.number = 0;
-        this.number = number;
-        this.display = display;
-    }
-
-    TileValue() {
-        this.number = 0;
-        this.display = false;
-    }
-
-    public int getNumber() {
-        return this.number;
-    }
-    
-    public boolean getDisplay() {
-        return this.display;
-    }
-
-    public void setDisplay(boolean displayValue) {
-        this.display = displayValue;
-    }
-}
-
 public class Tile {
     private final int id;
     public final TileValue value;
