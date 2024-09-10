ce8c7e5d2164e1eb581718b38d839084402152dc
Tsames <tsames1@gmail.com>
2024-09-09 22:31:45 -0700

    Added listToArray.py complete with a test case.

---
+++ Algorithms/LinkedLists/listToArray.py
@@ -0,0 +1,63 @@
+from listNode import Node
+'''
+❓ PROMPT
+Given a linked list, return an array with all the elements in reverse.
+
+Example(s)
+head = 1 -> 3 -> 5 -> 2
+createArrayInReverse(head) == [2,5,3,1]
+ 
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+ 
+
+Insightful & revealing test cases:
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1:
+Time: O()
+Space: O()
+ 
+
+📆 PLAN
+Outline of algorithm #: 
+ 
+
+🛠️ IMPLEMENT
+function createArrayInReverse(node) {
+def createArrayInReverse(node: Node) -> list[int]:
+ 
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+'''
+
+def createArrayInReverse(node: Node) -> list[int]:
+    res = []
+    
+    # Write data of each node to our result array
+    while node:
+        res.append(node.data)
+        node = node.next
+    
+    # Reverse our array
+    left, right = 0, len(res) - 1
+    while left < right:
+        res[left], res[right] = res[right], res[left]
+        left += 1
+        right -= 1
+    
+    return res
+
+testNodeHead = Node(1)
+testNodePointer = testNodeHead
+for i in range(2,10):
+    newNode = Node(i)
+    testNodePointer.next = newNode
+    testNodePointer = newNode
+
+print(createArrayInReverse(testNodeHead))
+    
