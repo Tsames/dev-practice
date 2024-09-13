3d833920c2e0bfbc991d10ed7c4f100b3575f1d5
Tsames <tsames1@gmail.com>
2024-09-13 14:26:24 -0700

    Refactored TileGroup class

--- Sudoku/TileGroup.java
+++ Sudoku/TileGroup.java
@@ -1,7 +1,5 @@
 package Sudoku;
 
-import java.util.HashMap;
-
 enum TileGroupType {
     row,
     column,
@@ -12,7 +10,6 @@ class TileGroup {
     private final int id;
     private final TileGroupType type;
     private final Tile[] tiles = new Tile[9];
-    private final HashMap<Integer, Boolean> values = new HashMap<Integer, Boolean>();
     
     public TileGroup(int id, TileGroupType type) {
         this.id = id;
@@ -31,24 +28,11 @@ class TileGroup {
         return this.tiles;
     }
 
-    public HashMap<Integer, Boolean> getValues() {
-        return this.values;
-    }
-
-    public void addTile(Tile newTile, int position) {
-        this.tiles[position] = newTile;
-        this.values.put(newTile.getValue(), true);
-    }
-
-    public boolean isValid() {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < this.tiles.length; i++) {
-            int tileValue = this.tiles[i].getValue();
-            if (tileValue != 0) {
-                if (map.containsKey(tileValue)) return false;
-                map.put(tileValue, 1);
+    public void removePossibleValueFromOtherTilesInGroup(int value, int positionInGroup) {
+        for (int i = 0; i < 9; i++) {
+            if (i != positionInGroup) {
+                tiles[i].removePossibleValue(value);
             }
         }
-        return true;
     }
 }
