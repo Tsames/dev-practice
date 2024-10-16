bb72e62c4691cfd22c2055a201c59f551eb5d767
Tsames <tsames1@gmail.com>
2024-10-15 19:08:45 -0700

    Added alternative solution to reorder_list.py

--- Algorithms/LinkedLists/reorder_list.py
+++ Algorithms/LinkedLists/reorder_list.py
@@ -5,15 +5,12 @@ Reorder Linked List
 You are given the head of a singly linked-list.
 
 The positions of a linked list of length = 7 for example, can intially be represented as:
-
 [0, 1, 2, 3, 4, 5, 6]
 
 Reorder the nodes of the linked list to be in the following order:
-
 [0, 6, 1, 5, 2, 4, 3]
 
 Notice that in the general case for a list of length = n the nodes are reordered to be in the following order:
-
 [0, n-1, 1, n-2, 2, n-3, ...]
 
 You may not modify the values in the list's nodes, but instead you must reorder the nodes themselves.
@@ -53,9 +50,50 @@ def reorder_with_linear_space(root: Node) -> Node:
     
     return root
 
-example_list = create_linked_list_from_list([1,2,3,4,5,6,7,8,9])
-print(example_list)
-reorder_with_linear_space(example_list)
-print(example_list)
+# example_list = create_linked_list_from_list([1,2,3,4,5,6,7,8,9])
+# print(example_list)
+# reorder_with_linear_space(example_list)
+# print(example_list)
 
+def reorder_list(root: Node) -> Node:
+    
+    # Find the middle of the linked list
+    slow_pointer = root
+    fast_pointer = root.next
+    
+    while fast_pointer and fast_pointer.next:
+        slow_pointer = slow_pointer.next
+        fast_pointer = fast_pointer.next.next
+    
+    # Reorder the second half of the linked list
+    reverse_list = slow_pointer.next
+    slow_pointer.next = None
+    
+    previous_node = None
+    
+    while reverse_list:
+        temp = reverse_list.next
+        reverse_list.next = previous_node
+        previous_node = reverse_list
+        if not temp:
+            break
+        reverse_list = temp
+    
+    # Combine two seperate lists into one, and return it
+    curr_list_one = root
+    curr_list_two = reverse_list
+    
+    while curr_list_one or curr_list_two:
+        temp = curr_list_one.next
+        curr_list_one.next = curr_list_two
+        curr_list_one = temp
         
+        temp = curr_list_two.next
+        curr_list_two.next = curr_list_one
+        curr_list_two = temp
+        
+    return root
+    
+
+example_list_two = create_linked_list_from_list([10,9,8,7,6,5,4,3,2,1])
+print(reorder_list(example_list_two))
