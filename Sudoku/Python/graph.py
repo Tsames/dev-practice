5b3d0381181e7981f3e67f97b0987091fdeb548d
Tsames <tsames1@gmail.com>
2024-10-29 17:49:54 -0700

    Put Graph class in its own file and made minor adjustments.

--- Sudoku/Python/graph.py
+++ Sudoku/Python/graph.py
@@ -1,86 +1,51 @@
 from typing import Optional
-
-
-class GraphNode:
-    
-    _connectedTo = set()
-    
-    def __init__(self, idx: int, val: Optional[int] = 0):
-        self._idx = idx
-        self.val = val
-
-    def getIndex(self) -> int:
-        return self._idx
-
-    def getVal(self) -> int:
-        return self._val
-
-    def setVal(self, newVal: int) -> None:
-        if 0 >= newVal or newVal > 9:
-            return
-        self._val = newVal
-
-    def getConnections(self) -> set["GraphNode"]:
-        return self._connectedTo.clone()
-
-    def addConnection(self, node: "GraphNode") -> None:
-        self._connectedTo.add(node)
-
-    def removeConnection(self, node: "GraphNode") -> None:
-        self._connectedTo.remove(node)
-
-    def hasConnection(self, node: "GraphNode") -> bool:
-        return node in self._connectedTo
-
-    def __str__(self):
-        return (
-            "Node "
-            + str(self.idx)
-            + f" [{self.data}] -"
-            + " Connected to: "
-            + str([x.idx for x in self.connectedTo])
-        )
+from graph_node import GraphNode
 
 
 class Graph:
 
-    numNodes = 0
-    allNodes = dict()
+    _allNodes = dict()
 
     def addNode(self, idx: int) -> Optional[GraphNode]:
-        if idx in self.allNodes or idx < 0 or idx > 80:
+        if idx < 0 or idx > 80 or idx in self._allNodes:
             return None
 
-        self.numNodes += 1
         newNode = GraphNode(idx)
-        self.allNodes[idx] = newNode
+        self._allNodes[idx] = newNode
         return newNode
-    
+
+    def getNode(self, idx: int) -> Optional[GraphNode]:
+        return self._allNodes[idx] if idx in self._allNodes else None
+
     def setNodeVal(self, idx, val) -> Optional[GraphNode]:
-        if idx not in self.allNodes:
-            return None
-        
-        self.allNodes[idx].setVal(val)
-
-    def addConnection(self, idxOne: int, idxTwo: int) -> None:
-        if idxOne not in self.allNodes or idxTwo not in self.allNodes:
+        if idx not in self._allNodes:
             return None
 
-        nodeOne = self.allNodes[idxOne]
-        nodeTwo = self.allNodes[idxTwo]
+        self._allNodes[idx].setVal(val)
 
-        nodeOne.addConnection(nodeTwo)
-        nodeTwo.addConnection(nodeOne)
-
-    def hasConnection(self, idxOne, idxTwo) -> bool:
+    def addEdge(self, idxOne: int, idxTwo: int) -> None:
         if (
-            idxOne not in self.allNodes
-            or idxTwo not in self.allNodes
-            or idxOne == idxTwo
+            idxOne == idxTwo
+            or idxOne not in self._allNodes
+            or idxTwo not in self._allNodes
         ):
-            return False
+            return None
 
-        nodeOne = self.allNodes[idxOne]
-        nodeTwo = self.allNodes[idxTwo]
+        nodeOne = self._allNodes[idxOne]
+        nodeTwo = self._allNodes[idxTwo]
+
+        nodeOne.addConnection(idxTwo)
+        nodeTwo.addConnection(idxOne)
+
+    def hasConnection(self, idxOne, idxTwo) -> Optional[bool]:
+        if (
+            idxOne == idxTwo
+            or idxOne not in self._allNodes
+            or idxTwo not in self._allNodes
+        ):
+            return None
+
+        nodeOne = self._allNodes[idxOne]
+        nodeTwo = self._allNodes[idxTwo]
 
         return nodeOne.hasConnection(nodeTwo) and nodeTwo.hasConnection(nodeOne)
