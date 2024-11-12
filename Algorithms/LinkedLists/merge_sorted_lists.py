a5cf5b87529871539fb85f641be3f52a914055f1
Tsames <tsames1@gmail.com>
2024-11-11 19:25:38 -0800

    Added merge_sorted_lists.py

---
+++ Algorithms/LinkedLists/merge_sorted_lists.py
@@ -0,0 +1,57 @@
+"""
+https://neetcode.io/problems/merge-two-sorted-linked-lists
+
+Merge Two Sorted Linked Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+The new list should be made up of nodes from list1 and list2.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,5]
+Output: [1,1,2,3,4,5]
+
+Example 2:
+Input: list1 = [], list2 = [1,2]
+Output: [1,2]
+
+Example 3:
+Input: list1 = [], list2 = []
+Output: []
+
+Constraints:
+0 <= The length of the each list <= 100.
+-100 <= Node.val <= 100
+"""
+
+from typing import Optional
+from listNode import ListNode, createFromList
+
+
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = curr = ListNode()
+
+        while l1 and l2:
+            if l1.data <= l2.data:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+
+        if l1:
+            curr.next = l1
+        else:
+            curr.next = l2
+
+        return dummy.next
+
+
+solution = Solution()
+print(
+    solution.mergeTwoLists(
+        createFromList([1, 3, 5, 7, 9]), createFromList([2, 4, 6, 8])
+    )
+)
