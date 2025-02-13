c37b25124fa12196184432ed5655c3ae4ee73e9e
Tsames <tsames1@gmail.com>
2025-02-13 09:04:27 -0800

    Cleaning up backtracking and heaps

--- Algorithms/Heaps/kth_largest_element.py
+++ Algorithms/Heaps/kth_largest_element.py
@@ -19,4 +19,19 @@ Constraints:
 1 <= k <= nums.length <= 10000
 -1000 <= nums[i] <= 1000
 """
+import heapq
 
+class Solution:
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        nums = [-x for x in nums]
+        heapq.heapify(nums)
+        
+        for i in range(k - 1):
+            heapq.heappop(nums)
+            
+        return -heapq.heappop(nums)
+        
+        
+solution = Solution()
+print(solution.findKthLargest([5,4,3,2,1], 3))
+        
